8593c2fe218c64ef298278593c29901a30f2c814
BHAJAN <bhajanlal1284@gmail.com>
2021-08-17 17:01:36 +0530

    modify

--- ecomm/homeapp/views.py
+++ ecomm/homeapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from productsapp.models import Product
+from productsapp.models import Product1
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'home/index.html', {})
+	Recent_pro = Product.objects.all()
+	elec_prod =  Product1.objects.all()
+	return render(request, 'home/index.html', {'Recent_pro':Recent_pro, 'elec_prod':elec_prod})
